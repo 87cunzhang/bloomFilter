2bb8ba0ca993bda83c8de266120214b98d8bdfdc
87cunzhang <2423561050@qq.com>
2023-04-14 16:11:45 +0800

    优化结构

--- BloomFilter.php
+++ BloomFilter.php
@@ -1,42 +1,64 @@
 <?php
+namespace BloomFilter;
+
 /**
- * Created by 村长
- * Date: 2023/3/19
- * Time: 11:39
+ * 布隆过滤器
+ * Class BloomFilter
+ * @author 靳文垒
  */
-
-namespace bloomFilter;
-
 class BloomFilter
 {
     private $redis;
-    private $hashFunctions;
     private $bitArraySize;
+    private $hashFunctions;
 
-    public function __construct($redis, $hashFunctions, $bitArraySize)
+    public function __construct($redis)
     {
-        $this->redis         = $redis;
-        $this->hashFunctions = $hashFunctions;
-        $this->bitArraySize  = $bitArraySize;
+        $this->redis = $redis;
+        // 位数组大小
+        $this->bitArraySize = 1 << 20;
+        // hash函数
+        $this->hashFunctions = [
+            function ($element) {
+                return crc32($element);
+            },
+            function ($element) {
+                return hexdec(substr(md5($element), 0, 8));
+            }
+        ];
     }
 
+
+    /**
+     * 添加元素
+     * @param $element
+     */
     public function add($element)
     {
         foreach ($this->hashFunctions as $hashFunction) {
             $bit = $hashFunction($element) % $this->bitArraySize;
-            echo "元素$element 的位置是: $bit\n";
             $this->redis->setbit('bloom_filter', $bit, 1);
+            echo "元素$element 的位置是: $bit\n";
         }
     }
 
+
+    /**
+     * 判断元素是否存在
+     * @param $element
+     * @return bool
+     */
     public function exists($element): bool
     {
         foreach ($this->hashFunctions as $hashFunction) {
             $bit = $hashFunction($element) % $this->bitArraySize;
+
             if (!$this->redis->getbit('bloom_filter', $bit)) {
                 return false;
             }
         }
+
         return true;
     }
+
 }
